7c8e9682a6e85eda20e971f7a19f7a6c2320af93
Bagusadi99 <163718900+Bagusadi99@users.noreply.github.com>
2024-03-17 22:24:38 +0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+  const startBtn = document.querySelector('.start-btn');
+const popupInfo = document.querySelector('.spopup-info');
+
+startBtn.onclick =() => {
+    popupInfo.classList.add('active');
+}
